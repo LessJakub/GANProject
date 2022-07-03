68f673261aead5afe8280808df496e8a4ca24870
LessJakub <kubafalalesniak@gmail.com>
2022-07-03 19:04:46 +0200

    Last modifications and code cleanup

--- StageOneDiscriminator.py
+++ StageOneDiscriminator.py
@@ -19,16 +19,11 @@ from numpy.random import default_rng
 from numpy import ones
 
 class StageOneDiscriminator(object):
-#def define_discriminator_stage_one(voc_size, max_length, image_shape=(64,64,3)):
     def __init__(self, img_shape=(64,64,3), text_shape=100):
         self.img_shape = img_shape
         self.text_shape = text_shape
         inputText = Input(shape=(text_shape,))
-        #change to 64*64*3 and hope for no OOM
-        #for 1024 it works but guesses everything as 0
         text_dense = Dense(512)(inputText)
-        #test next: text_pooling = MaxPooling1D(pool_size=2)(inputText)
-        #test next: text_dense = Dense(1024)(text_pooling)
    
 
         inputImage = Input(shape=img_shape)
@@ -65,43 +60,3 @@ class StageOneDiscriminator(object):
         opt = Adam(learning_rate=0.0002, beta_1=0.5)
         self.model = Model(inputs=[inputImage, inputText], outputs=y)
         self.model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
-        #return model
-
-    def train_discriminator(self,imgDataset, embDataset, n_iter=20, n_batch=128):
-        half_batch = int(n_batch / 2)
-        # manually enumerate epochs
-        for i in range(n_iter):
-		    # get randomly selected 'real' samples
-            X_real, y_real, l_real = generate_real_samples(imgDataset, embDataset, half_batch)
-		    # update discriminator on real samples
-            _, real_acc = self.model.train_on_batch((X_real, l_real), y_real)
-		    # generate 'fake' examples
-            X_fake, y_fake, l_fake = generate_real_samples(imgDataset, embDataset, half_batch)
-		    # update discriminator on fake samples
-            _, fake_acc = self.model.train_on_batch((X_fake, l_fake), y_fake)
-            # summarize performance
-            print('>%d real=%.0f%% fake=%.0f%%' % (i+1, real_acc*100, fake_acc*100))
-
-
-    def load_image(self, image_path):
-        img = tf.io.read_file(image_path)
-        img = tf.io.decode_jpeg(img, channels=self.img_shape[2])
-        img = tf.keras.layers.Resizing(self.img_shape[0], self.img_shape[1])(img)
-        img = tf.keras.applications.inception_v3.preprocess_input(img)
-        return img, image_path
-
-    def generate_real_samples(self, imageDataset, embedingsDataset, n_samples):
-        ix = randint(0, imageDataset.shape[0], n_samples)
-        images = []
-        labels = []
-
-        for i in ix:
-            img, _ = self.load_image(imageDataset[i])
-            images.extend(img)
-            labels.extend(embedingsDataset[i])
-
-        images = np.reshape(images, (n_samples, self.img_shape[0], self.img_shape[1], self.img_shape[2]))
-        labels = np.reshape(labels, (n_samples, self.text_shape,))
-        y = ones((n_samples, 1))
-
-        return images, labels, y
--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-#from StageOneDiscriminator import StageOneDiscriminator, define_discriminator_stage_one
-#from StageOneGenerator import StageOneGenerator, define_generator_stage_one
 from glove_loader import GloveModel
 
-
-#from StageOneGAN import StageOneGan, define_gan_stage_one
 from StageOneGAN import StageOneGAN
 
 from PIL import Image
@@ -16,12 +12,6 @@ import collections
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 from keras.models import load_model
-from load_birds import load_birds
-
-
-from numpy import asarray
-from numpy import savez_compressed
-from numpy import load
 
 
 
@@ -40,17 +30,9 @@ def load_image_two(image_path, img_shape=(64,64,3)):
         return img
 
 def main():
-    #IDEAS:
-    #1. Change discriminator to tanh activation
-    #        1 - Proper image
-    #        0 - Mismatched label
-    #       -1 - Wrong image wrong
-    #2. Go back to only 0 and 1
     gan_name = "coco"
     img_shape = (64,64,3)
-    paths_limit = 5
-    #seems to work on 200
-    #testing 300
+    paths_limit = 50
     text_size = 200
     latent_dim = 100
     epochs = 1000
@@ -65,12 +47,12 @@ def main():
         if not os.path.exists(os.path.abspath('.') + annotation_folder):
             annotation_zip = tf.keras.utils.get_file('captions.zip',
                                            cache_subdir=os.path.abspath('.'),
-                                           origin='http://images.cocodataset.org/annotations/annotations_trainval2017.zip',
+                                           origin='http://images.cocodataset.org/annotations/annotations_trainval2014.zip',
                                            extract=True)
-            annotation_file = os.path.dirname(annotation_zip)+'/annotations/captions_train2017.json'
+            annotation_file = os.path.dirname(annotation_zip)+'/annotations/captions_train2014.json'
             os.remove(annotation_zip)
 
-        annotation_file = os.getcwd() +'/annotations/captions_train2017.json'
+        annotation_file = os.getcwd() +'/annotations/captions_train2014.json'
 
         # Download image files
         image_folder = '/train2014/'
@@ -90,17 +72,12 @@ def main():
     
         with open(annotation_file, 'r') as f:
             annotations = json.loads(f.read())
-            print(json.dumps(annotations['categories']))
 
         for val in annotations['annotations']:
             caption = f"<start> {val['caption']} <end>"
             image_path = PATH + 'COCO_train2014_' + '%012d.jpg' % (val['image_id'])
             caption = caption.removeprefix("<start>").removesuffix("<end>")
             image_path_to_caption[image_path].append(caption)
-            #image_path_to_embedding[image_path].append(glove.encode_doc(caption))
-            #print(caption)
-        for cat in annotations['categories']:
-            print(cat)
 
         image_paths = list(image_path_to_caption.keys())
         random.shuffle(image_paths)
@@ -121,16 +98,6 @@ def main():
                 np.save(f, dataset)
     
         with open(train_dataset_file, 'rb') as f:
-            dataset = np.load(f, allow_pickle=True)
-
-    elif(gan_name == "birds"):
-        birds_dataset_file = "dataset_{}".format(gan_name)
-        if not os.path.exists(os.path.abspath('.') + "/" + birds_dataset_file):
-            dataset = load_birds()
-            random.shuffle(dataset)
-            with open(birds_dataset_file, 'wb') as f:
-                np.save(f, dataset)
-        with open(birds_dataset_file, 'rb') as f:
             dataset = np.load(f, allow_pickle=True)
     else:
         with open(train_dataset_file, 'rb') as f:
@@ -153,35 +120,12 @@ def main():
 
 
     ganOne = StageOneGAN(train_dataset, val_dataset, text_shape=text_size, latent_dim=latent_dim, image_shape=img_shape)
-    #g = load_model("models/generator_models_coco/generator_model_1000.h5")
     plt.imshow
 
 
     ganOne.init_model()
     ganOne.name = gan_name
     ganOne.train_two(n_epochs=epochs, n_batch=batch)
-
-
-    #ganTwo = StageTwoGAN()
-    #ganTwo.train(img_name_vector, train_embeddings, 1000, 8)
-    #ganOne.train(img_name_vector, train_embeddings, 4000, 64, 200)
-    #ganOne.train_birds(dic, filenames, n_batch=32)
-    #dTwo = StageTwoDiscriminator()
-    #plot_model(dTwo.model, to_file='d_two_plot.png', show_shapes=True, show_layer_names=True)
-    #gTwo = StageTwoGenerator()
-    #plot_model(gTwo.model, to_file='g_two_plot.png', show_shapes=True, show_layer_names=True)
-    # #plt.savefig('images/image_at_epoch_{:d}.png'.format(1))
-    # print("Creating GAN")
-    # g_model = define_generator_stage_one(vocabulary_size, max_length)
-    # g_model.load_weights('training_weightsTMP/gen_70.h5')
-    # plot_model(g_model, to_file='g_plot.png', show_shapes=True, show_layer_names=True)
-    # d_model = define_discriminator_stage_one(vocabulary_size, max_length)
-    # d_model.load_weights('training_weightsTMP/disc_70.h5')
-    # plot_model(d_model, to_file='d_plot.png', show_shapes=True, show_layer_names=True)
-    # #gan = define_gan_stage_one
-    # train(g_model, d_model, g_opt, d_opt, img_name_train, cap_train, index_to_word, 200, 32)
-    
-
 
 
 if __name__ == "__main__":
